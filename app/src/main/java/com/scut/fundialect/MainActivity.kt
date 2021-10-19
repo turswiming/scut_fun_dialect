6acff0ce72041b9865861a0ee0e7d85388836bb2
turswiming <1431839116@qq.com>
2021-10-19 16:09:20 +0800

    测试compose系统

--- app/src/main/java/com/scut/fundialect/MainActivity.kt
+++ app/src/main/java/com/scut/fundialect/MainActivity.kt
@@ -2,10 +2,13 @@ package com.scut.fundialect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.os.Message
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,6 +16,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.scut.fundialect.ui.theme.ComposeTutorialTheme
+
+
+data class Message(val author: String,val body:String)
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,16 +29,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContent {
-
+            ComposeTutorialTheme {
+                MessageCard(Message("Android", "Jetpack Compose"))
+            }
             Greeting(name = "lzq")
         }
     }
 }
+
 @Preview
 @Composable
 fun PreviewGreeting() {
-    Greeting("Android")
+    //Greeting("Android")
+    ComposeTutorialTheme {
+        MessageCard(
+            msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+        )
+
+    }
 }
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author ,
+                color = MaterialTheme.colors.secondaryVariant
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
+
+
 @Composable
 fun Greeting(name: String) {
     Row(
--- app/src/main/java/com/scut/fundialect/ui.theme/Theme.kt
+++ app/src/main/java/com/scut/fundialect/ui.theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun MyApplicationTheme(
+fun ComposeTutorialTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
