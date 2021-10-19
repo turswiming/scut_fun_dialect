f7b085cb8ac333f289e199e3c64ac9329e14fba2
turswiming <1431839116@qq.com>
2021-10-19 14:14:45 +0800

    再次添加了compose依赖。更新后需要在gradle中sync project， 请别改gradle，尤其是别改compose-version部分

--- app/src/main/java/com/scut/fundialect/MainActivity.kt
+++ app/src/main/java/com/scut/fundialect/MainActivity.kt
@@ -2,11 +2,22 @@ package com.scut.fundialect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setContent {
+            Greeting(name = "lzq")
+        }
     }
+}
+
+@Composable
+fun Greeting(name: String)
+{ Text(text = "Hello$name!")
 }
