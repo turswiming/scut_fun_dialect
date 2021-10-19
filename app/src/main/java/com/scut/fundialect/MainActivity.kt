c595e27a3a8b7aeca21ee2f69ec09bfdbe3f3296
turswiming <1431839116@qq.com>
2021-10-19 16:58:56 +0800

    测试compose的事件系统

--- app/src/main/java/com/scut/fundialect/MainActivity.kt
+++ app/src/main/java/com/scut/fundialect/MainActivity.kt
@@ -6,12 +6,17 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -30,15 +35,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContent {
-            ComposeTutorialTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+            ComposeTutorialTheme() {
+                Conversation(SampleData.conversationSample)
             }
-            Greeting(name = "lzq")
+            //Greeting(name = "lzq")
         }
     }
 }
 
 @Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme() {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+//@Preview
 @Composable
 fun PreviewGreeting() {
     //Greeting("Android")
@@ -62,8 +75,11 @@ fun MessageCard(msg: Message) {
         )
 
         Spacer(modifier = Modifier.width(8.dp))
-
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant ,
@@ -77,6 +93,10 @@ fun MessageCard(msg: Message) {
             ) {
                 Text(
                     text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
@@ -113,3 +133,12 @@ fun Greeting(name: String) {
 
 
 }
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn() {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
