7ff9b688012e74c148671a863e0670bbf577c53f
turswiming <1431839116@qq.com>
2021-10-23 16:39:24 +0800

    添加了数据库的表

--- app/src/main/java/com/scut/fundialect/MainActivity.kt
+++ app/src/main/java/com/scut/fundialect/MainActivity.kt
@@ -1,30 +1,11 @@
 package com.scut.fundialect
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import android.os.Message
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-//import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+
 import com.scut.fundialect.helpClass.MyDataBaseHelper
-import com.scut.fundialect.helpClass.SampleData
-import com.scut.fundialect.ui.theme.ComposeTutorialTheme
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 data class Message(val author: String,val body:String)
@@ -36,119 +17,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setContent {
-            ComposeTutorialTheme() {
-                Conversation(SampleData.conversationSample)
-            }
+
             val dbHelper = MyDataBaseHelper(this,"main.db",1)
-            dbHelper.writableDatabase
+        val db = dbHelper.writableDatabase
             //Greeting(name = "lzq")
 
+        mainButtonTryDataBase.setOnClickListener {
+            val value1 = ContentValues().apply {
+                put("userNickName", "The Da Vinci Code")
+                put("userMail", "Dan Brown@qq.com")
+                put("userPassport", "3423423")
+                put("userSex", 1)
+                put("userCityId", 0)
 
-
-        }
-    }
-}
-
-
-
-
-@Preview
-@Composable
-fun PreviewConversation() {
-    ComposeTutorialTheme() {
-        Conversation(SampleData.conversationSample)
-    }
-}
-
-//@Preview
-@Composable
-fun PreviewGreeting() {
-    //Greeting("Android")
-    ComposeTutorialTheme {
-        MessageCard(
-            msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
-        )
-
-    }
-}
-@Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = null,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
-
-        Spacer(modifier = Modifier.width(8.dp))
-        // We keep track if the message is expanded or not in this
-        // variable
-        var isExpanded by remember { mutableStateOf(false) }
-        // We toggle the isExpanded variable when we click on this Column
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant ,
-                style = MaterialTheme.typography.subtitle2
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                elevation = 1.dp
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    // If the message is expanded, we display all its content
-                    // otherwise we only display the first line
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.body2
-                )
             }
+            db.insert("userinfo",null,value1)
+        }
 
         }
     }
-}
 
-
-@Composable
-fun Greeting(name: String) {
-    Row(
-        modifier = Modifier.padding(all = 9.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "图片测试",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column(
-
-        ) {
-            Text(text = "Hello$name!",
-                    modifier =  Modifier.
-                    padding(all = 8.dp)
-            )
-            Text(text = "宁来啦！")
-            Text(text = "$name 我等您好久啦")
-        }
-    }
-
-
-}
-@Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn() {
-        items(messages) { message ->
-            MessageCard(message)
-        }
-    }
-}
 
--- app/src/main/java/com/scut/fundialect/helpClass/MyDataBaseHelper.kt
+++ app/src/main/java/com/scut/fundialect/helpClass/MyDataBaseHelper.kt
@@ -1,21 +1,27 @@
 package com.scut.fundialect.helpClass
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.widget.Toast
-
+enum class sex{
+    male,
+    female
+}
 class MyDataBaseHelper(val context: Context,name:String,version:Int):
     SQLiteOpenHelper(context,name,null,version) {
-    private val createBook = "create table Book (" +
+    private val createUserInfo = "create table userinfo (" +
             " id integer primary key autoincrement," +
-            "author text," +
-            "price real," +
-            "pages integer," +
-            "name text)"
+            "userNickName text," +
+            "userMail text," +
+            "userPassport text," +
+            "userSex integer," +
+            "userCityId integer" +
+            ")"
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL(createBook)
+        db?.execSQL(createUserInfo)
         Toast.makeText(context,"创建成功",Toast.LENGTH_SHORT).show()
 
     }
