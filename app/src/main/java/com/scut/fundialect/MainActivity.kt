633e0cb9cfa90f48763f70411638ed02599b80a1
turswiming <1431839116@qq.com>
2021-10-25 17:54:16 +0800

    完善图片处理类

--- app/src/main/java/com/scut/fundialect/MainActivity.kt
+++ app/src/main/java/com/scut/fundialect/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.core.content.FileProvider
 import com.scut.fundialect.database.UserInfoDataBaseHelper
 
 import com.scut.fundialect.database.VideoDataBaseHelper
+import com.scut.fundialect.help.PicManager
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
@@ -30,65 +31,34 @@ class MainActivity : AppCompatActivity() {
     lateinit var imageUri: Uri
     lateinit var outputImage: File
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         takePhotoBtn.setOnClickListener {
-            // 打开文件选择器
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-            intent.addCategory(Intent.CATEGORY_OPENABLE)
-            // 指定只显示图片
-            intent.type = "image/*"
-            startActivityForResult(intent, fromAlbum)
+            gitPic(fromAlbum)
         }
+   }
+
+    private fun MainActivity.gitPic(fromAlbum: Int) {
+        // 打开文件选择器
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        // 指定只显示图片
+        intent.type = "image/*"
+        startActivityForResult(intent, fromAlbum)
+
     }
-override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     super.onActivityResult(requestCode, resultCode, data)
-    when (requestCode) {
-        fromAlbum -> {
-            if (resultCode == Activity.RESULT_OK && data != null) {
-                data.data?.let { uri ->
-                    // 将选择的图片显示
-                    val bitmap = getBitmapFromUri(uri)
-                    imageView.setImageBitmap(bitmap)
-                }
-            }
-        }
+        PicManager.getPicUri(requestCode, resultCode, data,contentResolver)
+}
 
-        takePhoto -> {
-            if (resultCode == Activity.RESULT_OK) {
-                // 将拍摄的照片显示出来
-                val bitmap = BitmapFactory.decodeStream(contentResolver.
-                openInputStream(imageUri))
-                imageView.setImageBitmap(rotateIfRequired(bitmap))
-            }
-        }
-    }
-}
-    private fun getBitmapFromUri(uri: Uri) = contentResolver
-        .openFileDescriptor(uri, "r")?.use {
-            BitmapFactory.decodeFileDescriptor(it.fileDescriptor)
-        }
 
-    private fun rotateIfRequired(bitmap: Bitmap): Bitmap {
-    val exif = ExifInterface(outputImage.path)
-    val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
-        ExifInterface.ORIENTATION_NORMAL)
-    return when (orientation) {
-        ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90)
-        ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180)
-        ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270)
-        else -> bitmap
-    }
-}
-private fun rotateBitmap(bitmap: Bitmap, degree: Int): Bitmap {
-    val matrix = Matrix()
-    matrix.postRotate(degree.toFloat())
-    val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height,
-        matrix, true)
-    bitmap.recycle() // 将不再需要的Bitmap对象回收
-    return rotatedBitmap
-}
+
+
+
 }
 
 
--- app/src/main/java/com/scut/fundialect/help/PicManager.kt
+++ app/src/main/java/com/scut/fundialect/help/PicManager.kt
@@ -1,18 +1,92 @@
 package com.scut.fundialect.help
 
+import android.R.attr
+import android.app.Activity
+import android.content.ContentResolver
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.startActivityForResult
+import com.scut.fundialect.MainActivity
+import com.scut.fundialect.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import java.io.File
+import java.net.URI
 import java.text.SimpleDateFormat
 import java.util.*
+import android.R.attr.path
+import android.content.Context
+import androidx.core.net.toUri
 
-class PicManager {
-    inner class Pic(uri: Uri)
-    val defaulturi = ""
 
+object PicManager {
+    val takePhoto = 1
+    val fromAlbum = 2
+    public fun getPicUri(
+        requestCode: Int, resultCode: Int, data: Intent?,
+        contentResolver: ContentResolver
+    ): Uri {
+        when (requestCode) {
+            fromAlbum -> {
+                if (resultCode == AppCompatActivity.RESULT_OK && data != null) {
+                    data.data?.let {
+                        //这玩意儿就是我们梦寐以求的uri
+                            uri ->
+                        // 将选择的图片显示
+                        val bitmap = getBitmapFromUri(uri, contentResolver)
+                        return uri
+                    }
+                }
+            }
 
+        }
+
+        return defaultUri()
+
+    }
+
+    private fun defaultUri() =
+        Uri.parse("android.resource://" + "com.scut.fundialect" + "/" + R.raw.defaultPic)
+
+    private fun getBitmapFromUri(uri: Uri, contentResolver: ContentResolver) {
+        return contentResolver.openFileDescriptor(uri, "r").use {
+            BitmapFactory.decodeFileDescriptor(it?.fileDescriptor)
+        }
+    }
+
+    public fun CopyPic(outuri: Uri,context:Context): Uri {
+        //为啥这要封装一层？
+        //您猜，哎嘿
+        return copyFile(outuri,context)
+    }
+
+    fun copyFile(uri: Uri,context:Context):Uri {
+        lateinit var file: File
+        val fromFile =  File(URI(uri.toString()));
+        fromFile.runCatching {
+            takeIf {
+                it.exists()
+            }?.inputStream()?.use {
+                    inputStream ->
+                file = context.getFilesDir()
+                file =File(file.path,"name")
+                file.outputStream().use {
+                        outputStream ->
+                    inputStream.copyTo(outputStream)
+                }
+            }
+        }.onFailure {
+            return defaultUri()
+        }
+        return file.toUri()
+    }
 }
+
+
