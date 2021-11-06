266c082d555b9c4fcdad9dd4193fd072336358bd
turswiming <1431839116@qq.com>
2021-11-06 13:07:10 +0800

    添加了数据库测试

--- app/src/main/java/com/scut/fundialect/activity/MainActivity.kt
+++ app/src/main/java/com/scut/fundialect/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -17,7 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.scut.fundialect.R
-import com.scut.fundialect.database.SampleData
+import com.scut.fundialect.database.*
 import com.scut.fundialect.help.PicManager
 import com.scut.fundialect.help.VideoHelper
 import com.scut.fundialect.ui.theme.ComposeTutorialTheme
@@ -40,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         takePhotoBtn.setOnClickListener {
             PicManager.gitPicFromAlbum(this)
         }
+        test()
    }
 
 
@@ -49,6 +51,26 @@ class MainActivity : AppCompatActivity() {
         val uri = PicManager.getPicUri(requestCode, resultCode, data,contentResolver)
         imageView.setImageURI(uri)
     }
+    fun test(){
+        val cityDataBaseHelper = CityDataBaseHelper(this,"city.db",1)
+        val cityDB = cityDataBaseHelper.writableDatabase
+        Toast.makeText(this, "cityDB初始化成功", Toast.LENGTH_SHORT).show()
+
+        val dubedVideoDataBaseHelper =DubedVideoDataBaseHelper(this,"dubed.db",1)
+        val dubedDB =dubedVideoDataBaseHelper.writableDatabase
+        Toast.makeText(this, "dubedDB初始化成功", Toast.LENGTH_SHORT).show()
+
+        val learnVideoDataBaseHelper = LearnVideoDataBaseHelper(this,"Learn.db",1)
+        val learnDB =dubedVideoDataBaseHelper.writableDatabase
+        Toast.makeText(this, "LearnDB初始化成功", Toast.LENGTH_SHORT).show()
+
+        val modelVideoDataBaseHelper =ModelVideoDataBaseHelper(this,"Model.db",1)
+        val ModelDB =dubedVideoDataBaseHelper.writableDatabase
+        Toast.makeText(this, "ModelDB初始化成功", Toast.LENGTH_SHORT).show()
+
+        val userInfoDataBaseHelper = UserInfoDataBaseHelper(this,"userinfo.db",1)
+        val userinfoDB = userInfoDataBaseHelper.writableDatabase
+
+
+    }
 }
-
-
