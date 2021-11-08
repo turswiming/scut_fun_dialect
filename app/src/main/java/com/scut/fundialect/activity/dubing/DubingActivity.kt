c8e49e15fe787d2d0de6ea8b949a2cec650814d7
turswiming <1431839116@qq.com>
2021-11-08 20:09:27 +0800

    添加了预制页面

---
+++ app/src/main/java/com/scut/fundialect/activity/dubing/DubingActivity.kt
@@ -0,0 +1,38 @@
+package com.scut.fundialect.activity.dubing
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.scut.fundialect.activity.dubing.ui.theme.FunDialectTheme
+
+class DubingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FunDialectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    FunDialectTheme {
+        Greeting2("Android")
+    }
+}
---
+++ app/src/main/java/com/scut/fundialect/activity/myself/LearnActivity.kt
@@ -0,0 +1,38 @@
+package com.scut.fundialect.activity.myself
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.scut.fundialect.activity.myself.ui.theme.FunDialectTheme
+
+class LearnActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FunDialectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting4("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting4(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview4() {
+    FunDialectTheme {
+        Greeting4("Android")
+    }
+}
